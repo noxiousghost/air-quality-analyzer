f2154aa2e2c224ba6ce057a78e53ab465b9e86ba
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-10-02 22:40:40 +0545

    fixed status reference error

--- src/configs/error.config.ts
+++ src/configs/error.config.ts
@@ -1,11 +1,9 @@
 export class AppError extends Error {
   statusCode: number;
-  status: string;
   isOperational: boolean;
   constructor(message: string, statusCode: number) {
     super(message);
     this.statusCode = statusCode;
-    this.status = status;
     this.isOperational = true;
     Error.captureStackTrace(this, this.constructor);
   }
