a0a7099f13b45d63e04cea7091a7bbc3a0eda738
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-10-15 19:27:55 +0545

    created basic landing page

--- src/configs/express.config.ts
+++ src/configs/express.config.ts
@@ -1,6 +1,7 @@
 import express, { Express } from 'express';
 import cors from 'cors';
 import { envVars } from './envVars.config';
+import path from 'path';
 
 const app: Express = express();
 app.disable('x-powered-by');
@@ -8,7 +9,7 @@ app.disable('x-powered-by');
 const corsOptions = {
   origin: [envVars.Access_Control_Allow_Origin_URL as string],
 };
-
+app.use(express.static(path.join(__dirname, '..', '..', 'public')));
 app.use(cors(corsOptions));
 app.use(express.json());
 
